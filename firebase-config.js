34de5c410cfed84663b8e2fd1762deaa09d243b8
marvin-santos-sawce <contato.marvin19@gmail.com>
2026-08-28 09:28:53 -0300

    ajuste para voltar a funcionar

--- firebase-config.js
+++ firebase-config.js
@@ -8,16 +8,7 @@
 // Veja o passo a passo completo no README.md deste projeto.
 // ============================================================
 
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import {initializeApp} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
-  import {getAnalytics} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+export const firebaseConfig = {
     apiKey: "AIzaSyDZDEJza82gJpPYXrEI1xJVozmOTfuJMz0",
   authDomain: "selfkill-counter.firebaseapp.com",
   databaseURL: "https://selfkill-counter-default-rtdb.firebaseio.com",
